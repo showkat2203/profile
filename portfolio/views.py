39dc925f78a34b33ac7d7bd8737d44294da3b7f0
showkat2203 <showkat2203@diu.edu.bd>
2020-11-14 00:32:54 +0600

    added required Models, post, comments, categories.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
+from portfolio.models import Project, Post, Comment
 
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'index.html', context)
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog-detail.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog-detail.html", context)
