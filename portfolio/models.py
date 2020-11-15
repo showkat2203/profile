c013f496e0e1fa6baff1d6c37d96cacfa88db813
showkat2203 <showkat2203@diu.edu.bd>
2020-11-15 20:39:27 +0600

    Added comment form

--- portfolio/models.py
+++ portfolio/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+    image = models.FilePathField(path="/img")
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from portfolio.models import Project, Post, Comment
+from portfolio.forms import CommentForm
 
 def index(request):
     projects = Project.objects.all()
@@ -15,12 +16,34 @@ def blog_index(request):
     }
     return render(request, "blog-detail.html", context)
 
+# def blog_detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     comments = Comment.objects.filter(post=post)
+#     context = {
+#         "post": post,
+#         "comments": comments,
+#     }
+
+#     return render(request, "blog-detail.html", context)
+
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form,
     }
-
-    return render(request, "blog-detail.html", context)
+    return render(request, "blog_detail.html", context)
